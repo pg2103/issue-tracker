a4cfd6d0df135bd0bde0bdceb808fd66a251eb31
Prakhar Gupta <prakhar.gupta.212003@gmail.com>
2024-07-15 00:12:53 +0530

    added a markdown editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import './globals.css'
 import '@radix-ui/themes/styles.css';
+import './theme-config.css'
 import { Theme } from '@radix-ui/themes';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+   subsets: ['latin'],
+   variable: '--font-inter',
+   })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,12 +22,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" >
+      <body  className={inter.variable}>
+        <Theme accentColor="violet">
           <NavBar/>
           <main className='p-5'>{children}</main>
         </Theme>
+        
       </body>
     </html>
   )
