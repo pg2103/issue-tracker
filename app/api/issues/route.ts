96ecea1778670a27b7b32504da72d575359605d3
Prakhar Gupta <prakhar.gupta.212003@gmail.com>
2024-07-20 23:22:34 +0530

    Secure the application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,9 +1,15 @@
+import authOptions from '@/app/auth/authOptions';
 import { issueSchema } from '@/app/validationSchemas';
 import prisma from "@/prisma/client";
+import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function POST(request: NextRequest) {
+    const session=await getServerSession(authOptions)
+    if(!session){
+        return NextResponse.json({},{status:401})
+    }
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
     if (!validation.success) {
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,11 +4,14 @@ import { notFound } from 'next/navigation'
 import DeleteIssueButton from './DeleteIssueButton'
 import EditIssueButton from './EditIssueButton'
 import IssueDetails from './IssueDetails'
+import { getServerSession } from 'next-auth'
+import authOptions from '@/app/auth/authOptions'
 
 interface Props{
     params:{id:string}
 }
 const IssueDetailPage =async ({params}:Props) => {
+    const session=await getServerSession(authOptions)
     const issue=await prisma.issue.findUnique({
         where:{
             id:parseInt(params.id)
@@ -23,10 +26,10 @@ const IssueDetailPage =async ({params}:Props) => {
             <IssueDetails issue={issue} />    
         </Box>
         <Box>
-            <Flex direction="column" gap="4">
+            {session&&<Flex direction="column" gap="4">
                 <EditIssueButton issueId={issue.id}/>
                 <DeleteIssueButton issueId={issue.id}/>
-            </Flex>
+            </Flex>}
         </Box>
     </Grid>
   )
