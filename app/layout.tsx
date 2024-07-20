cc4540693e46a57dd0172afae681a166813f2538
Prakhar Gupta <prakhar.gupta.212003@gmail.com>
2024-07-21 00:05:05 +0530

    Setup React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 import AuthProvider from './auth/Provider';
+import QueryClientProvider from './QueryClientProvider';
 
 const inter = Inter({
    subsets: ['latin'],
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <body  className={inter.variable}>
+        <QueryClientProvider>
         <AuthProvider>
         <Theme accentColor="violet">
           <NavBar/>
@@ -35,6 +37,7 @@ export default function RootLayout({
           </main>
         </Theme>
         </AuthProvider>
+        </QueryClientProvider>
         
       </body>
     </html>
