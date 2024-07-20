ddbb19a71639a012371c212ff799af028a720124
Prakhar Gupta <prakhar.gupta.212003@gmail.com>
2024-07-20 10:14:09 +0530

    Add a dropdown menu

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { AiFillBug } from 'react-icons/ai'
 import {useSession} from 'next-auth/react'
-import { Box, Container, Flex } from '@radix-ui/themes'
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes'
 
 const NavBar = () => {
     const currentPath=usePathname()
@@ -38,7 +38,25 @@ const NavBar = () => {
                 </Flex>
                 <Box>
                     {status==='authenticated' && (
-                    <Link href='/api/auth/signout'>Log Out</Link>
+                    <DropdownMenu.Root>
+                        <DropdownMenu.Trigger>
+                            <Avatar src={session.user!.image!} 
+                            fallback="?"
+                            size='2'
+                            radius='full'
+                            />
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Content>
+                            <DropdownMenu.Label>
+                                <Text size='2'>
+                                    {session.user!.email}
+                                </Text>
+                            </DropdownMenu.Label>
+                            <DropdownMenu.Item>
+                                <Link href='/api/auth/signout'>Logout</Link>
+                            </DropdownMenu.Item>
+                        </DropdownMenu.Content>
+                    </DropdownMenu.Root>
                     )}
                     {status==='unauthenticated' && (
                     <Link href='/api/auth/signin'>Login</Link>
